89e94b2e900b5389881f15da30c3094abd58f662
VietLH <vietlh@VietLHs-MacBook-Pro.local>
2019-06-27 20:06:25 +0700

    fix based on review from Apple

--- NightOwlCam/ViewController.swift
+++ NightOwlCam/ViewController.swift
@@ -10,6 +10,7 @@
 import UIKit
 import AVFoundation
 import AVKit
+import Photos
 
 class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AVAudioRecorderDelegate, UIScrollViewDelegate {
     
@@ -17,6 +18,7 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
     @IBOutlet var viewCover: UIView!
     @IBOutlet var imgCenterLogo: UIImageView!
     @IBOutlet var swtSplit: UISwitch!
+    @IBOutlet var imgIndicator: UIImageView!
     
     @IBOutlet var scvTutorial: UIScrollView!
     @IBOutlet var btnTutorial: UIButton!
@@ -31,11 +33,17 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
     var cameraPreviewLayer:AVCaptureVideoPreviewLayer?
     var captureAudioDevice :AVCaptureDevice?
     
+    var isReadyToRecord = false
     var isRecording = false
+    var wasInterruptedByPuttingToBackground = false
     var toggleCameraGestureRecognizer = UISwipeGestureRecognizer()
     
     var zoomInGestureRecognizer = UISwipeGestureRecognizer()
     var zoomOutGestureRecognizer = UISwipeGestureRecognizer()
+    var scaleUpIndicatorGestureRecognizer = UISwipeGestureRecognizer()
+    var scaleDownIndicatorGestureRecognizer = UISwipeGestureRecognizer()
+    var enableIndicatorScalingGestureRecognizer = UITapGestureRecognizer()
+    
     
     var tapGestureRecognizer = UITapGestureRecognizer()
     var doubleTapGestureRecognizer = UITapGestureRecognizer()
@@ -51,6 +59,13 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
     let fileA = "outputA.mov"
     let fileB = "outputB.mov"
     var isEnableSplitVideo = false
+    var indicatorScalingRatio = 1.0
+    var isIndicatorScalingEnable = false
+    
+    var settings = [String:String]()
+    let indicatorScalingRatioKey = "indicatorScalingRatio"
+    let enableSplitVideoKey = "splitVideo"
+    
     
     override var prefersStatusBarHidden: Bool {
         return true
@@ -79,6 +94,14 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
         zoomOutGestureRecognizer.addTarget(self, action: #selector(zoomOut))
         view.addGestureRecognizer(zoomOutGestureRecognizer)
         
+        scaleUpIndicatorGestureRecognizer.direction = .left
+        scaleUpIndicatorGestureRecognizer.addTarget(self, action: #selector(scaleUpIndicator))
+        viewCover.addGestureRecognizer(scaleUpIndicatorGestureRecognizer)
+        
+        scaleDownIndicatorGestureRecognizer.direction = .right
+        scaleDownIndicatorGestureRecognizer.addTarget(self, action: #selector(scaleDownIndicator))
+        viewCover.addGestureRecognizer(scaleDownIndicatorGestureRecognizer)
+        
         tapGestureRecognizer.addTarget(self, action: #selector(tapCount))
         viewCover.addGestureRecognizer(tapGestureRecognizer)
         
@@ -86,6 +109,11 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
         doubleTapGestureRecognizer.numberOfTouchesRequired = 1
         doubleTapGestureRecognizer.addTarget(self, action: #selector(triggerStopRecord))
         viewCover.addGestureRecognizer(doubleTapGestureRecognizer)
+        
+        enableIndicatorScalingGestureRecognizer.numberOfTapsRequired = 3
+        enableIndicatorScalingGestureRecognizer.numberOfTouchesRequired = 2
+        enableIndicatorScalingGestureRecognizer.addTarget(self, action: #selector(enableIndicatorScaling))
+        viewCover.addGestureRecognizer(enableIndicatorScalingGestureRecognizer)
         
         tapLogoGestureRecognizer.addTarget(self, action: #selector(countLogoTap))
         imgCenterLogo.addGestureRecognizer(tapLogoGestureRecognizer)
@@ -96,14 +124,15 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
             self.btnTutorial.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
         }, completion: nil)
         scvTutorial.delegate = self
-        //        var files = [String]()
-        //        do{
-        //            files = try FileManager.default.contentsOfDirectory(atPath: NSTemporaryDirectory())
-        //        } catch {
-        //            print(error)
-        //            return
-        //        }
         
+        readingAppSettings()
+        NotificationCenter.default.addObserver(self, selector: #selector(saveDataBeforeQuit), name: UIApplication.willResignActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(resumeAppProgress), name: UIApplication.willEnterForegroundNotification, object: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkAuthorizationForAccessingMedia()
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
@@ -131,7 +160,6 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
                     }
                 }
                 if(device.hasMediaType(AVMediaType.audio)){
-                    print("Capture device audio init")
                     captureAudioDevice = device //initialize audio
                 }
             }
@@ -172,12 +200,17 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
     }
     
     @IBAction func capture(sender: UIButton) {
-        if !isRecording {
-            isRecording = true
-        } else {
-            isRecording = false
+        if isReadyToRecord {
+            if !self.isRecording {
+                self.isRecording = true
+            } else {
+                self.isRecording = false
+            }
+            self.changeRecordState()
+        }else{
+            openAppSettings()
         }
-        changeRecordState()
+        
     }
     
     @IBAction func btnGotitAct(_ sender: Any) {
@@ -195,8 +228,10 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
     @IBAction func swtChangeAct(_ sender: Any) {
         if swtSplit.isOn {
             isEnableSplitVideo = true
+            settings[enableSplitVideoKey] = "1"
         }else{
             isEnableSplitVideo = false
+            settings[enableSplitVideoKey] = "0"
         }
     }
     
@@ -221,20 +256,27 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
                 self.recordButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
             }, completion: nil)
             recordButton.layer.removeAllAnimations()
+            imgCenterLogo.layer.removeAllAnimations()
+            imgIndicator.layer.removeAllAnimations()
             videoFileOutput?.stopRecording()
             enableCover(now: false)
+            writingAppSettings()
         }
     }
     
     // MARK: - AVCaptureFileOutputRecordingDelegate methods
     func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
         if error != nil {
-            print(error!)
+            if wasInterruptedByPuttingToBackground {
+                wasInterruptedByPuttingToBackground = false
+                print("recording video")
+                UISaveVideoAtPathToSavedPhotosAlbum(outputFileURL.relativePath, self, nil, nil)
+            }
+            print(error!.localizedDescription + "\nvideoFile: \(outputFileURL.lastPathComponent)")
             return
         }
-        //        performSegue(withIdentifier: "playVideo", sender: outputFileURL)
-        let videoFileURL = outputFileURL
-        UISaveVideoAtPathToSavedPhotosAlbum(videoFileURL.relativePath, self, nil, nil)
+        print("recording video")
+        UISaveVideoAtPathToSavedPhotosAlbum(outputFileURL.relativePath, self, nil, nil)
         if isRecording{
             switchOutput()
         }
@@ -305,6 +347,30 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
         }
     }
     
+    @objc func scaleUpIndicator(){
+        if isIndicatorScalingEnable{
+            if indicatorScalingRatio < 1 {
+                indicatorScalingRatio *= 2
+                if indicatorScalingRatio > 1 {
+                    indicatorScalingRatio = 1
+                }
+            }
+            scaleIndicator()
+        }
+    }
+    
+    @objc func scaleDownIndicator(){
+        if isIndicatorScalingEnable{
+            if indicatorScalingRatio > 0.1 {
+                indicatorScalingRatio *= 0.5
+                if indicatorScalingRatio < 0.1 {
+                    indicatorScalingRatio = 0.1
+                }
+            }
+            scaleIndicator()
+        }
+    }
+    
     @objc func finish() {
     }
     
@@ -312,6 +378,7 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
         if now {
             recordButton.isHidden = true
             imgCenterLogo.isHidden = false
+            imgIndicator.isHidden = true
             viewCover.alpha = 1
             UIApplication.shared.isIdleTimerDisabled = true
             
@@ -329,7 +396,6 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
             UIView.animate(withDuration: 0.5, delay: 1.0, options: [], animations: { () -> Void in
                 self.imgCenterLogo.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
             }, completion: nil)
-            imgCenterLogo.layer.removeAllAnimations()
         }
     }
     
@@ -339,6 +405,9 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
             Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(rejectTriggerStopRecord), userInfo: nil, repeats: false)
             tick = requiredTick
             tickLimit = requiredTickLimit
+            UIView.animate(withDuration: 0.5, delay: 0.0, options: [.repeat, .autoreverse, .allowUserInteraction], animations: { () -> Void in
+                self.imgCenterLogo.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            }, completion: nil)
             imgCenterLogo.isHidden = false
             imgCenterLogo.isUserInteractionEnabled = true
         }
@@ -369,6 +438,10 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
         imgCenterLogo.isHidden = true
         imgCenterLogo.isUserInteractionEnabled = false
         viewCover.isUserInteractionEnabled = true
+        scaleIndicator()
+        imgIndicator.isHidden = false
+        imgCenterLogo.transform = CGAffineTransform.identity
+        rotateView(targetView: imgIndicator, duration: 0.5)
     }
     
     @objc func countLogoTap(){
@@ -428,12 +501,192 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate, AV
         }
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "playVideo" {
-            let videoPlayerViewController = segue.destination as! AVPlayerViewController
-            let videoFileURL = sender as! URL
-            videoPlayerViewController.player = AVPlayer(url: videoFileURL)
-            UISaveVideoAtPathToSavedPhotosAlbum(videoFileURL.relativePath, self, nil, nil)
+    func checkAuthorizationForAccessingMedia() {
+        // handler in .requestAccess is needed to process user's answer to our request
+        var videoCheckpoint = false
+        var audioCheckpoint = false
+        var photosCheckpoint = false
+        var isAlertShowing = false
+        
+        
+        let cameraStatus = AVCaptureDevice.authorizationStatus(for: .video)
+        if cameraStatus == .notDetermined {
+            AVCaptureDevice.requestAccess(for: .video) { success in
+                if success { // if request is granted (success is true)
+                    videoCheckpoint = true
+                } else { // if request is denied (success is false)
+                    
+                }
+            }
+        }else{
+            if cameraStatus == .authorized {
+                videoCheckpoint = true
+            }else {
+                if !isAlertShowing {
+                    isAlertShowing = true
+                    openAppSettings()
+                }
+            }
         }
+        
+        let audioStatus = AVCaptureDevice.authorizationStatus(for: .audio)
+        if audioStatus == .notDetermined {
+            AVCaptureDevice.requestAccess(for: .audio) { success in
+                if success { // if request is granted (success is true)
+                    audioCheckpoint = true
+                } else { // if request is denied (success is false)
+                    
+                }
+            }
+        } else {
+            if audioStatus == .authorized {
+                audioCheckpoint = true
+            }else{
+                if !isAlertShowing {
+                    isAlertShowing = true
+                    openAppSettings()
+                }
+            }
+        }
+        
+        //check access to Photos
+        let photosStatus = PHPhotoLibrary.authorizationStatus()
+        if photosStatus == .notDetermined {
+            print("request access to photos")
+            PHPhotoLibrary.requestAuthorization({status in
+                if status == .authorized{
+                    photosCheckpoint = true
+                } else {
+                    
+                }
+            })
+        } else {
+            if (photosStatus == PHAuthorizationStatus.authorized) {
+                // Access has been granted.
+                photosCheckpoint = true
+            } else if (photosStatus == PHAuthorizationStatus.denied) {
+                // Access has been denied.
+                if !isAlertShowing {
+                    isAlertShowing = true
+                    openAppSettings()
+                }
+            }
+        }
+        
+        if(audioCheckpoint && photosCheckpoint && videoCheckpoint){
+            isReadyToRecord = true
+        }
+    }
+    
+    func openAppSettings() {
+        let alert = UIAlertController(title: "Alert", message: "Please authorize access to Camera, Microphone and Photos before recording video.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Go to Settings", style: .default, handler: { action in
+            let settingsUrl = URL(string:UIApplication.openSettingsURLString)
+            if let url = settingsUrl {
+                if UIApplication.shared.canOpenURL(url) {
+                    if #available(iOS 10.0, *) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    } else {
+                        UIApplication.shared.openURL(url)
+                    }
+                }
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func scaleIndicator() {
+        settings[indicatorScalingRatioKey] = String(indicatorScalingRatio)
+        imgIndicator.transform = CGAffineTransform(scaleX: CGFloat(indicatorScalingRatio), y: CGFloat(indicatorScalingRatio))
+    }
+    
+    private func rotateView(targetView: UIView, duration: Double = 1.0) {
+        UIView.animate(withDuration: duration, delay: 0.0, options: .curveLinear, animations: {
+            targetView.transform = targetView.transform.rotated(by: CGFloat.pi)
+        }) { finished in
+            self.rotateView(targetView: targetView, duration: duration)
+        }
+    }
+    
+    @objc func enableIndicatorScaling() {
+        if !isIndicatorScalingEnable {
+            isIndicatorScalingEnable = true
+            Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(disableIndicatorScaling), userInfo: nil, repeats: false)
+        }
+    }
+    
+    @objc func disableIndicatorScaling() {
+        if isIndicatorScalingEnable {
+            isIndicatorScalingEnable = false
+        }
+    }
+    
+    func readingAppSettings() {
+        let fileName = "settings.txt"
+        let filePath = NSTemporaryDirectory() + fileName
+        let fileURL = URL(fileURLWithPath: filePath)
+        var settingContent = ""
+        let fileMan = FileManager.default
+        if fileMan.fileExists(atPath: filePath ) {
+            do{
+                settingContent = try String(contentsOf: fileURL)
+            }catch{
+                print(error)
+            }
+        }
+        for setting in settingContent.split(separator: "\n") {
+            let key = String(setting.split(separator: ":")[0])
+            let value = String(setting.split(separator: ":")[1])
+            settings[key] = value
+        }
+        
+        if settings[indicatorScalingRatioKey] != nil {
+            indicatorScalingRatio = Double(settings[indicatorScalingRatioKey]!)!
+        }else{
+            indicatorScalingRatio = 1.0
+        }
+        if settings[enableSplitVideoKey] != nil {
+            if settings[enableSplitVideoKey] == "1"{
+                isEnableSplitVideo = true
+            }else{
+                isEnableSplitVideo = false
+            }
+        }else{
+            isEnableSplitVideo = false
+        }
+    }
+    
+    func writingAppSettings() {
+        let fileName = "settings.txt"
+        let filePath = NSTemporaryDirectory() + fileName
+        let fileURL = URL(fileURLWithPath: filePath)
+        var settingContent = ""
+        
+        for key in settings.keys {
+            let setting = key + ":" + settings[key]! + "\n"
+            settingContent += setting
+        }
+        do{
+            try settingContent.write(to: fileURL, atomically: false, encoding: String.Encoding.utf8)
+        }catch{
+            print(error)
+        }
+    }
+    
+    //set of actions that need to take before puting the app to background
+    @objc func saveDataBeforeQuit(){
+        if isRecording{
+            UISaveVideoAtPathToSavedPhotosAlbum(videoFileURL!.relativePath, self, nil, nil)
+            wasInterruptedByPuttingToBackground = true
+            print("urgent saving")
+        }
+        writingAppSettings()
+        isRecording = false
+        changeRecordState()
+        enableCover(now: false)
+    }
+    
+    //set of behaviours that need to perform when resuming from background
+    @objc func resumeAppProgress(){
     }
 }
